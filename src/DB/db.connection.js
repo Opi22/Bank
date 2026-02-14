2a87d82dc4de38ac9735dc6b29269bda1bfed18a
Opi22 <khideromer20031610@gmail.com>
2026-02-14 15:31:25 +0200

    successfully made firs run

--- src/DB/db.connection.js
+++ src/DB/db.connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const dbConnect= async()=>{
-    await mongoose.connect(uri).then(()=>{
+    await mongoose.connect("mongodb://localhost:27017/Bank").then(()=>{
         console.log("db connected successfully")
     }).catch((err)=>{
         console.log(err);
--- src/app.controller.js
+++ src/app.controller.js
@@ -1,5 +1,6 @@
+import dbConnect from "./DB/db.connection.js";
 const bootsrap= (app, express)=>{
     app.use(express.json())
-
+    dbConnect()
 }
 export default bootsrap;
